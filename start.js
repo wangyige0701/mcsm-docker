7c7649dc5f3bc05c09502462437d66a3454c82ed
WangYige <1776686515@qq.com>
2024-09-01 11:59:20 +0800

    语法调整

--- start.js
+++ start.js
@@ -1,19 +1,25 @@
 const { spawn } = require('child_process');
 
 function start (sh) {
-    const terminal = spawn('bash', [
-        '-c',
-        `cd ${process.env.MCSM_PATH} && bash ${sh}.sh`
-    ]);
-
-    terminal.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    terminal.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
+    return new Promise((resolve, reject) => {
+        const terminal = spawn('bash', [
+            '-c',
+            `cd ${process.env.MCSM_PATH} && bash ${sh}.sh`
+        ]);
+    
+        terminal.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+            resolve();
+        });
+    
+        terminal.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+            reject();
+        });
+    })
 }
 
-start('start-daemon');
-start('start-web');
+(async () => {
+    await start('start-daemon');
+    await start('start-web');
+})()
